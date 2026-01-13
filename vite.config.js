9b715e2d537f9d6440b6fd26220cf97bc58a3d75
xtruel <xtruel@users.noreply.github.com>
2026-01-13 10:02:48 +0100

    Update vite config for cache busting

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    base: "./"
+    base: "./",
+    build: {
+        rollupOptions: {
+            output: {
+                entryFileNames: `assets/[name]-${Date.now()}.js`,
+                chunkFileNames: `assets/[name]-${Date.now()}.js`,
+                assetFileNames: `assets/[name]-${Date.now()}.[ext]`
+            }
+        }
+    }
 });
